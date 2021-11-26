5b73c80dd0254ca6366b93362004b41aba7578e5
kayahan <albayrakkayahan@gmail.com>
2021-11-27 02:29:14 +0300

    cors configuration

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
 app.use(express.json());
+
+const whitelist = ["https://kayahan-todo-list-react.herokuapp.com/"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
 
 
 app.use('/', require('./routes/routes'));
